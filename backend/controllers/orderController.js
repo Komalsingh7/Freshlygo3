108d0c2b27514232e3c14d787680ef54f9d5ebd0
Komalsingh7 <162954877+Komalsingh7@users.noreply.github.com>
2025-06-20 13:04:18 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,7 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
 import stripe from "stripe";
+import User from  "../models/User.js";
 
 export const placeOrderCOD = async(req,res) => {
       try {
@@ -86,6 +87,49 @@ export const placeOrderStripe = async(req,res) => {
          return res.json({success:false , message:error.message});
       }
 }
+export const stripeWebhooks = async (request , response) => {
+   const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+   const sig = request.headers["stripe-signature"];
+   let event;
+   try {
+      event = stripeInstance.webhooks.constructEvent(
+          request.body,
+          sig,
+          process.env.STRIPE_WEBHOOK_SECRET
+      );
+   } catch (error) {
+       response.status(400).send(`webhook Error : ${error.message}`);
+   } 
+   switch (event.type) {
+    case "payment_intent.succeeded":{
+       const paymentIntent = event.data.object;
+       const paymentIntentId = paymentIntent.id;
+       const session = await stripeInstance.checkout.sessions.list({
+          payment_intent : paymentIntentId,
+       });
+       const {orderId , userId} = session.data[0].metadata;
+       await Order.findByIdAndUpdate(orderId, {isPaid:true})
+       await User.findByIdAndUpdate(userId ,{cartItems:{}})
+        break;
+    }
+      
+      case "payment_intent.payment_failed":{
+       const paymentIntent = event.data.object;
+       const paymentIntentId = paymentIntent.id;
+       const session = await stripeInstance.checkout.sessions.list({
+          payment_intent : paymentIntentId,
+       });
+       const {orderId} = session.data[0].metadata;
+       await Order.findByIdAndDelete(orderId);
+       break;
+      }
+    default: 
+       console.error(`Unhandled event type ${event.type}`)
+      break;
+   }
+   response.json({received:true});
+}
+
 
 export const getUserOrders = async(req, res) => {
   try {
