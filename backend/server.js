284a154f5d241322d2a101798f9fd1481d83adc2
Komalsingh7 <162954877+Komalsingh7@users.noreply.github.com>
2025-06-20 13:05:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,13 @@ import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
-
+import { stripeWebhooks } from './controllers/orderController.js';
 
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 const allowedOrigins = ['http://localhost:5173'];
-
+app.post('/stripe' , express.raw({type:'application/json'}) , stripeWebhooks)
 await connectDB();
 await connectCloudinary();
 
